11e03fa9d32666fa22142da7925d6b58f4b82438
Sylvain HENRY <hsyl20@gmail.com>
2015-01-15 10:43:01 +0100

    Minor style improvements

--- System/Posix/DynamicLinker/Template.hs
+++ System/Posix/DynamicLinker/Template.hs
@@ -1,20 +1,25 @@
-{-# LANGUAGE TemplateHaskell, CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module System.Posix.DynamicLinker.Template (
-  makeDynamicLinker, Callconv(..), DL, id, FunPtr
-  ) where
-
-import Language.Haskell.TH.Syntax
-import Control.Monad (liftM, when, unless, liftM2, void, join)
-import Data.List (nub)
-import Data.Functor ( (<$>), fmap )
+module System.Posix.DynamicLinker.Template
+   ( makeDynamicLinker
+   , Callconv(..)
+   , DL
+   , FunPtr
+  )
+where
 
 import System.Posix.DynamicLinker
+
+import Language.Haskell.TH.Syntax
+
+import Control.Monad (when, unless, join)
 import Foreign.Ptr
 import Foreign.C.String
-import Data.Traversable(traverse)
-import Data.Map (fromList,lookup)
-import Data.Maybe (fromMaybe, isNothing, fromJust,Maybe)
+import Data.Traversable (traverse)
+import Data.Foldable (traverse_)
+import Data.Map (fromList, lookup)
+import Data.Maybe (isNothing, fromJust, Maybe)
+import Data.Functor (fmap)
 
 -- | Generate dynamic linking FFI methods for each field in the first parameter
 makeDynamicLinker :: Name -- ^ Name of the data type
@@ -24,19 +29,20 @@ makeDynamicLinker :: Name -- ^ Name of the data type
 makeDynamicLinker t callconv symMod = do
   info <- reify t
   reified <- case info of
-                TyConI dec -> return dec
-                _ -> fail $ errmsg t
+     TyConI dec -> return dec
+     _ -> fail $ errmsg t
 
   (name,cons) <- case reified of
-               DataD [] _ [] [RecC name cons'] _ -> return (name,cons')
-               NewtypeD [] _ [] (RecC name cons') _ -> return (name,cons')
-               _ -> fail $ errmsg t
+     DataD [] _ [] [RecC name cons'] _ -> return (name,cons')
+     NewtypeD [] _ [] (RecC name cons') _ -> return (name,cons')
+     _ -> fail $ errmsg t
 
   -- Check for a field named "libHandle"
-  unless (any (\(Name name _,_,_) -> occString name == "libHandle")  cons) $ qReport True (nolibhandlemsg t)
+  unless (any ((==) "libHandle" . extractName) cons) $
+     qReport True (nolibhandlemsg t)
 
   -- Exclude "libHandle" from the symbol list
-  let symbols = filter (\(Name name _,_,_) -> occString name /= "libHandle") cons
+  let symbols = filter ((/=) "libHandle" . extractName) cons
 
   maybeType <- [t| Data.Maybe.Maybe |]
   funptr <- [t| Foreign.Ptr.FunPtr |]
@@ -47,7 +53,7 @@ makeDynamicLinker t callconv symMod = do
   let symbolsE = ListE $ map (\ (Name occ _) -> LitE $ StringL $ occString occ) names
 
   -- Generate names for foreign import functions
-  makes <- mapM (\((Name occ _),_,_) -> newName $ "make_" ++ occString occ) symbols
+  makes <- mapM (newName . ("make_" ++) . extractName) symbols
 
   -- Generate foreign calls
   foreigns <- mapM (\ (n,t,mk) -> makeForeign funptr n t mk) (zip3 names realTypes makes)
@@ -68,10 +74,12 @@ makeDynamicLinker t callconv symMod = do
 
     loadName = transformNameLocal ("load" ++) t
 
+    extractName (Name name _, _, _) = occString name
+
     -- | Transform a name using the given function
     transformName :: (String -> String) -> Name -> Name
-    transformName namer (Name occ f) = Name newName f
-      where newName = mkOccName $ namer $ occString occ
+    transformName namer (Name occ f) = Name newNam f
+      where newNam = mkOccName $ namer $ occString occ
 
     -- | Transform a name using the given function and make it local
     transformNameLocal :: (String -> String) -> Name -> Name
@@ -82,7 +90,8 @@ makeDynamicLinker t callconv symMod = do
     -- | Generate a foreign declaration
     makeForeign :: Type -> Name -> Type -> Name -> Q Dec
     makeForeign fptr name typ mk = do
-      return . ForeignD $ ImportF callconv Safe "dynamic" mk (AppT (AppT ArrowT (AppT fptr typ)) typ)
+      let importTyp = AppT (AppT ArrowT (AppT fptr typ)) typ
+      return (ForeignD (ImportF callconv Safe "dynamic" mk importTyp))
 
     -- | Generate module loader function
     makeLoader :: Name -> [Name] -> [Bool] -> [Name] -> Exp -> Q [Dec]
@@ -104,14 +113,15 @@ makeDynamicLinker t callconv symMod = do
             -- Associative Map: Modified symbol name -> Ptr () (may be null)
             let symPtrs = fromList $ modSymbols `zip` ptrs
 
-            let fromFunPtr a = if a == nullFunPtr then Nothing else Just a
 
             -- Modified symbol name -> Maybe (Ptr a) 
-            let pick a = join $ fmap (fromFunPtr . castFunPtr) $ Data.Map.lookup a symPtrs
+            let 
+               fromFunPtr a = if a == nullFunPtr then Nothing else Just a
+               pick a = join $ fmap (fromFunPtr . castFunPtr) $ Data.Map.lookup a symPtrs
+               missingmsg name lib = "Mandatory symbol \"" ++ name ++ "\" was not found in " ++ lib
+               checkSym (name,opt) = when (not opt && isNothing (pick name)) $ error (missingmsg name lib)
 
-            void $ traverse (\(name,opt) -> do
-                when (not opt && isNothing (pick name)) $ error ("Mandatory symbol \"" ++ name ++ "\" was not found in " ++ lib)
-              ) (zip modSymbols $(lift $ optionals))
+            traverse_ checkSym (zip modSymbols $(lift $ optionals))
 
             -- Fill the structure
             return $( do
@@ -123,18 +133,23 @@ makeDynamicLinker t callconv symMod = do
                 return $ RecConE t (handleField:fds)
               ) 
         |]
+      
+      -- loadXXX signature
       sigType <- [t| FilePath -> [RTLDFlags] -> IO $(return $ ConT $ transformNameLocal id t) |]
+      -- loadXXX body
       let load = FunD loadName [Clause [] (NormalB body) []]
+      -- return declarations
       return [SigD loadName sigType,load]
-
-    literalize (Name occ _) = LitE $ StringL $ occString occ -- Get a string literal from a name
+ 
+    -- Get a string literal from a name
+    literalize (Name occ _) = LitE $ StringL $ occString occ
 
     -- | Create a record field for a symbol
     makeField :: Name -> Bool -> Exp -> Exp -> Name -> Q FieldExp
     makeField mk isOptional pick fm name = do
       op <- if isOptional then [| id |] else [| fromJust |]
 
-      return (name, AppE op $ AppE (AppE fm (VarE mk)) $ AppE pick $ AppE (VarE symMod) $ literalize name)
+      return (name, AppE op . AppE (AppE fm (VarE mk)) . AppE pick . AppE (VarE symMod) $ literalize name)
 
 
     nodefmsg t = "Warning: No dynamic linker method generated from the name " ++ show t
@@ -145,3 +160,4 @@ makeDynamicLinker t callconv symMod = do
            ++ "\n it is not a type declared with 'data' or 'newtype'"
            ++ "\n Did you remember to double-tick the type as in"
            ++ "\n $(makeDynamicLinker ''TheType)?"
+
